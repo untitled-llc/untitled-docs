326a1ec969919905de80ab1c5121058125ef7f72
Noah Gary <noah@untitledfirm.com>
2020-12-16 15:20:45 -0500

    remove swap file

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Untitled: ['canopy-getting-started','canopy'],
+    Canopy: ['canopy-getting-started','canopy-system-architecture','canopy-environments','canopy-dev-flow','canopy-changelog','canopy-runbook'],
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx', 'untitDocs'],
   },
